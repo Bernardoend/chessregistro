516194c957e8a369881264c075cfae455f153dfa
Bernardoend <113403237+Bernardoend@users.noreply.github.com>
2023-05-10 10:44:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-echo "Olá, turma do az900";
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Minha página inicial</title>
+  </head>
+  <body>
+    <h1>Minha página inicial</h1>
+    <p>welcome to los pollos hermanos, how can i help you?.</p>
+    <p>sou um aluno iniciante em programação e cloud computing. Gosto também de reassistir especifícamente a 4 temporada de brbad, ouvir musica, futezinho sexta e cs go</p>
+    <a href="#" class="button">Entre em contato</a>
+  </body>
+</html>
