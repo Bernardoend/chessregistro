13bc81aa26289dd033c525132585363bf7cf73f1
Bernardoend <113403237+Bernardoend@users.noreply.github.com>
2023-05-10 10:52:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html>
   <head>
     <title>Minha página inicial</title>
+    <link rel="stylesheet" href="personalizacao.css">
   </head>
   <body>
     <h1>Minha página inicial</h1>
     <p>welcome to los pollos hermanos, how can i help you?.</p>
-    <p>sou um aluno iniciante em programação e cloud computing. Gosto também de reassistir especifícamente a 4 temporada de brbad, ouvir musica, futezinho sexta e cs go</p>
+    <p>sou um aluno iniciante em programação e cloud computing. Gosto também de reassistir especifícamente as 5 temporadas de brbad, ouvir musica, futezinho sexta e cs go</p>
     <a href="#" class="button">Entre em contato</a>
   </body>
 </html>
