849c9a1d801390e6a5e068a202079eb329c8d069
Bernardoend <113403237+Bernardoend@users.noreply.github.com>
2023-05-10 11:38:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
     <h1>Minha página inicial</h1>
     <p>welcome to los pollos hermanos, how can i help you?.</p>
     <p>sou um aluno iniciante em programação e cloud computing. Gosto também de reassistir especifícamente as 5 temporadas de brbad, ouvir musica, futezinho sexta e cs go</p>
-    <a href="https://www.youtube.com/@oraluca" target= "_blank"class="button">Entre em contato</a>
+    <a href="https://www.youtube.com/watch?v=RS4ZzYjFZcE" target= "_blank"class="button">Entre em contato</a>
+    <a href="https://cantodacasa.azurewebsites.net/" target= "_blank"class="button">susica</a>
     <div class="css">
  <img src="https://i.pinimg.com/videos/thumbnails/originals/7d/82/1b/7d821b3e87d673c6e78c63b66ab0e793.0000000.jpg" alt="Eduardo meme" title="Eduardo" width="150" height="150" />
 </div>
