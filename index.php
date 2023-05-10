cd3f9bbce7a452874c385d7b7f8a324c03e7b771
Bernardoend <113403237+Bernardoend@users.noreply.github.com>
2023-05-10 12:03:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
     <h1>Minha página inicial</h1>
     <p>welcome to los pollos hermanos, how can i help you?.</p>
     <p>sou um aluno iniciante em programação e cloud computing. Gosto também de reassistir especifícamente as 5 temporadas de brbad, ouvir musica, futezinho sexta e cs go</p>
+    <?php
+    date_default_timezone_set
+      ('America/sao_Paulo');
+    echo date('h:i:s A');
+    ?>
     <a href="https://www.youtube.com/watch?v=RS4ZzYjFZcE" target= "_blank"class="button">Entre em contato</a>
     <a href="https://cantodacasa.azurewebsites.net/" target= "_blank"class="button">susica</a>
     <div class="css">
